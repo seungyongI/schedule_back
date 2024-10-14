2448877b521233c91f35a803b57388384a69df24
DESKTOP-SCJ83VA\user <coding0063@gmail.com>
2024-10-14 17:40:07 +0900

    project 생성

---
+++ dailyLog/src/test/java/com/example/dailyLog/DailyLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dailyLog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
