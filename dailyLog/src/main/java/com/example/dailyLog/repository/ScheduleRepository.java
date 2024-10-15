190313da7a9b001797dcf5c51556d9aff6a11549
j_eun0415 <j_eun0415@naver.com>
2024-10-15 23:01:56 +0900

    Schedule Service & Repository update

--- dailyLog/src/main/java/com/example/dailyLog/repository/ScheduleRepository.java
+++ dailyLog/src/main/java/com/example/dailyLog/repository/ScheduleRepository.java
@@ -3,5 +3,12 @@ package com.example.dailyLog.repository;
 import com.example.dailyLog.entity.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+
+    List<Schedule> findByStartBetween(LocalDateTime start, LocalDateTime end);
+
 }
