caaf47a1713905c920227bd2f405989abd2a86ff
j_eun0415 <j_eun0415@naver.com>
2024-10-21 23:58:37 +0900

    Kakao login class created

---
+++ dailyLog/src/main/java/com/example/dailyLog/filter/JWTInspectFilter.java
@@ -0,0 +1,21 @@
+package com.example.dailyLog.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@Slf4j
+public class JWTInspectFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        filterChain.doFilter(request, response);
+    }
+}
