4069c0f65eed72dff7927383f4f24365dadcb7fa
DESKTOP-SCJ83VA\user <tackteho@naver.com>
2024-12-16 14:19:42 +0900

    일정 조회시 end date범위 수정시 나오지 않는 오류 관련 dto수정, 쿼리 메소드 수정

--- dailyLog/src/main/java/com/example/dailyLog/repository/ScheduleRepository.java
+++ dailyLog/src/main/java/com/example/dailyLog/repository/ScheduleRepository.java
@@ -18,8 +18,13 @@ public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
     List<Schedule> findByCalendarsIdxAndStartBetween(@Param("calendarIdx") Long calendarIdx, @Param("start") LocalDateTime start, @Param("end") LocalDateTime end);
 
     // 일간 일정 조회
-    @Query("SELECT s FROM Schedule s WHERE s.start >= :start AND s.end <= :end AND s.calendars.idx = :calendarIdx")
-    List<Schedule> findSchedulesInDay(@Param("start") LocalDateTime start, @Param("end") LocalDateTime end, @Param("calendarIdx") Long calendarIdx);
+    @Query("SELECT s FROM Schedule s WHERE "
+            + "((s.start <= :end AND s.end >= :start) AND s.calendars.idx = :calendarIdx)")
+    List<Schedule> findSchedulesInDay(
+            @Param("start") LocalDateTime start,
+            @Param("end") LocalDateTime end,
+            @Param("calendarIdx") Long calendarIdx
+    );
 
     List<Schedule> findByStartBetween(LocalDateTime start, LocalDateTime end);
 
